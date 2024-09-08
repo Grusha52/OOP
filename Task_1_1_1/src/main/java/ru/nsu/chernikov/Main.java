1cc53286ddfc8cefc94fa7a41661eed0c8da9370
Chernikov Grigory <grisha.kenig@gmail.com>
2024-09-08 16:39:03 +0700

    checkstyle fixed

--- Task_1_1_1/src/main/java/ru/nsu/chernikov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/chernikov/Main.java
@@ -5,59 +5,59 @@ import java.util.Scanner;
 public class Main {
 
 
-        public static void  heapify(int[] arr, int n, int i){
-            int Big = i;
-            int L = 2*i + 1;
-            int R = 2*i + 2;
-            if (L < n && arr[L] > arr[Big]) {
-                Big = L;
-            }
-            if (R < n && arr[R] > arr[Big]) {
-                Big = R;
-            }
-            if (Big != i)
-            {
-                int tmp = arr[i];
-                arr[i] = arr[Big];
-                arr[Big] = tmp;
-                heapify(arr, n, Big);
-            }
-
+    public static void heapify(int[] arr, int n, int i) {
+        int Big = i;
+        int L = 2 * i + 1;
+        int R = 2 * i + 2;
+        if (L < n && arr[L] > arr[Big]) {
+            Big = L;
+        }
+        if (R < n && arr[R] > arr[Big]) {
+            Big = R;
+        }
+        if (Big != i) {
+            int tmp = arr[i];
+            arr[i] = arr[Big];
+            arr[Big] = tmp;
+            heapify(arr, n, Big);
         }
 
-        public static int[] heapsort (int[] arr){
-            int n = arr.length;
+    }
 
-            for (int i = (n / 2 - 1); i >= 0; i--){
-                heapify(arr,n,i);
-            }
-            for (int i = n-1; i>=0; i--){
-                int tmp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = tmp;
-                heapify(arr,i,0);
-            }
+    public static int[] heapsort(int[] arr) {
+        int n = arr.length;
 
-            return (arr);
-
+        for (int i = (n / 2 - 1); i >= 0; i--) {
+            heapify(arr, n, i);
         }
-        public  static void printArray (int[] arr, int n){
-            for (int i = 0; i < n; i++){
-                System.out.print(arr[i] + ",");
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            int tmp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = tmp;
+            heapify(arr, i, 0);
         }
 
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            System.out.println("Enter numbers which you want to sort");
-            String input = in.nextLine();
-            String[] splitNumbers = input.split(",");
-            int count = splitNumbers.length;
-            int[] numbers = new int[count];
-            for (int i = 0; i<count; i++) {
-                numbers[i] = Integer.parseInt(splitNumbers[i]);
-            }
-            heapsort(numbers);
-            printArray(numbers,count);
+        return (arr);
+
+    }
+
+    public static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter numbers which you want to sort");
+        String input = in.nextLine();
+        String[] splitNumbers = input.split(",");
+        int count = splitNumbers.length;
+        int[] numbers = new int[count];
+        for (int i = 0; i < count; i++) {
+            numbers[i] = Integer.parseInt(splitNumbers[i]);
+        }
+        heapsort(numbers);
+        printArray(numbers, count);
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/chernikov/test.java
+++ Task_1_1_1/src/test/java/ru/nsu/chernikov/test.java
@@ -2,23 +2,27 @@ package ru.nsu.chernikov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-class Heaptests {
+class test {
     @Test
     void heaptest() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Main.heapsort(new int[] {5,4,3,2,1}));
-        assertArrayEquals(new int[]{-5,3,3,4,5,5,6,6,6,7,8,9,9,34}, Main.heapsort(new int[] {6,5,6,3,3,4,5,7,8,6,9,9,34,-5}));
-        assertArrayEquals(new int[]{-6543,-234,-21,-12,0,4,5,5,5,5,5,5,5,5,5,6,6,12,12,12,12,21,23,23,32,34,35,37,123,234,452,534,646,4543,8680}, Main.heapsort(new int[] {5,4,5,6,5,34,23,452,534,6,37,-234,234,12,32,35,646,8680,4543,23,123,-12,-6543,5,5,5,5,5,5,12,12,12,21,-21,0}));
-        assertArrayEquals(new int[]{}, Main.heapsort(new int[] {}));
-        assertArrayEquals(new int[]{1,1,1,1,1,1}, Main.heapsort(new int[] {1,1,1,1,1,1}));
-        assertArrayEquals(new int[]{-500,-250,0,250,500}, Main.heapsort(new int[] {250,0,-500,-250,500}));
-
-
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Main.heapsort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{-5, 3, 3, 4, 5, 5, 6, 6, 6, 7, 8, 9, 9, 34}, Main.heapsort(new int[]{6, 5, 6, 3, 3, 4, 5, 7, 8, 6, 9, 9, 34, -5}));
+        assertArrayEquals(
+                new int[]{-6543, -234, -21, -12, 0, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 12, 12, 12, 12, 21, 23, 23, 32, 34, 35, 37, 123, 234, 452, 534, 646, 4543, 8680},
+                Main.heapsort(new int[]{5, 4, 5, 6, 5, 34, 23, 452, 534, 6, 37, -234, 234, 12, 32, 35, 646, 8680, 4543, 23, 123, -12, -6543, 5, 5, 5, 5, 5, 5, 12, 12, 12, 21, -21, 0}));
+        assertArrayEquals(new int[]{}, Main.heapsort(new int[]{}));
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1, 1}, Main.heapsort(new int[]{1, 1, 1, 1, 1, 1}));
+        assertArrayEquals(new int[]{-500, -250, 0, 250, 500}, Main.heapsort(new int[]{250, 0, -500, -250, 500}));
+        assertArrayEquals(new int[]{0}, Main.heapsort(new int[]{0}));
+        assertArrayEquals(new int[]{0}, Main.heapsort(new int[]{0}));
+        assertArrayEquals(new int[]{-1000}, Main.heapsort(new int[]{-1000}));
+        assertArrayEquals(new int[]{10000}, Main.heapsort(new int[]{100000}));
+        assertArrayEquals(new int[]{0}, Main.heapsort(new int[]{0}));
 
 
     }
-
 
 
 }
