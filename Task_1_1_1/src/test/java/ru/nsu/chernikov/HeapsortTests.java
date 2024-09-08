96d3a6c1761b45776fc55f1df06b1c00426b33f8
Chernikov Grigory <grisha.kenig@gmail.com>
2024-09-08 17:29:04 +0700

    tests updated v2

--- Task_1_1_1/src/test/java/ru/nsu/chernikov/HeapsortTests.java
+++ Task_1_1_1/src/test/java/ru/nsu/chernikov/HeapsortTests.java
@@ -41,13 +41,13 @@ class HeapsortTests {
 
         assertArrayEquals(new int[]{0}, Main.heapsort(new int[]{0}));
 
-        assertArrayEquals(new int[]{0,1,2,3,9999,10000}, Main.heapsort(new int[]{9999,3,10000,2,1,0}));
+        assertArrayEquals(new int[]{0, 1, 2, 3, 9999, 10000}, Main.heapsort(new int[]{9999, 3, 10000, 2, 1, 0}));
 
-        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, Main.heapsort(new int[]{1,2,3,4,5,6,7,8,9}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, Main.heapsort(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
 
-        assertArrayEquals(new int[]{-9,-8,-7,-6,-5,-4,-3,-2,-1,0}, Main.heapsort(new int[]{-9,-5,-7,-6,-8,-4,-3,0,-1,-2}));
+        assertArrayEquals(new int[]{-9, -8, -7, -6, -5, -4, -3, -2, -1, 0}, Main.heapsort(new int[]{-9, -5, -7, -6, -8, -4, -3, 0, -1, -2}));
 
-        assertArrayEquals(new int[]{-9,-8,-7,-6,-5,-4,-3,-2,-1,0}, Main.heapsort(new int[]{-9,-8,-7,-6,-5,-4,-3,-2,-1,0}));
+        assertArrayEquals(new int[]{-9, -8, -7, -6, -5, -4, -3, -2, -1, 0}, Main.heapsort(new int[]{-9, -8, -7, -6, -5, -4, -3, -2, -1, 0}));
         
     }
 
