17755f2739d1f9bca7c93cad2bb758507b5bbb1b
Grigory Chernikov <g.chernikov@g.nsu.ru>
2024-11-21 13:55:19 +0700

    lab done?

---
+++ Task_1_3_1/src/main/java/ru/nsu/chernikov/SubstringFinding.java
@@ -0,0 +1,70 @@
+package ru.nsu.chernikov;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class SubstringFinding {
+    public static ArrayList<Long> find(String file, String substring) throws IOException {
+        ArrayList<Long> indexes = new ArrayList<>();
+        byte[] sub = substring.getBytes(StandardCharsets.UTF_8);
+        byte[] buf = new byte[sub.length + 1];
+        int overlapping = sub.length - 1;
+        byte[] overlapbuffer = new byte[overlapping];
+        int bytesread;
+        byte[] chunk = new byte[sub.length * 2];
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+            long symbolPointer = -1;
+            boolean firstIt = true;
+
+            while ((bytesread = raf.read(buf)) != -1) {
+
+                if (!firstIt) {
+                    System.arraycopy(overlapbuffer, 0, chunk, 0, overlapping);
+                    System.arraycopy(buf, 0, chunk, overlapping, buf.length);
+                } else {
+                    Arrays.fill(overlapbuffer, (byte) 0x81);
+                    System.arraycopy(overlapbuffer, 0, chunk, 0, overlapping);
+                    System.arraycopy(buf, 0, chunk, overlapping, buf.length);
+                    firstIt = false;
+                }
+
+                for (int i = 0; i < bytesread; i++) {
+
+                    if ((chunk[i] & 0xC0) != 0x80) {
+                        symbolPointer++;
+
+                        boolean flag = true;
+
+                        for (int j = 0; j < sub.length; j++) {
+                            if (chunk[i + j] != sub[j]) {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            indexes.add(symbolPointer);
+                        }
+                    }
+
+
+                }
+                System.arraycopy(chunk, chunk.length - overlapping, overlapbuffer, 0, overlapping);
+            }
+        }
+        return indexes;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<Long> result = find("file.txt", "аа");
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Task_1_3_1/src/test/java/ru/nsu/chernikov/SubstringFindingTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.chernikov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubstringFindingTest {
+    @Test
+    void main(){
+        SubstringFinding.main(null);
+    }
+  
+}
