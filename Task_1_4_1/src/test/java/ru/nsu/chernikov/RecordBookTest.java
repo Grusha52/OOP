467e444f43475db99d1811cbf9ee1fcb5eaa6372
Grigory Chernikov <g.chernikov@g.nsu.ru>
2024-11-29 10:10:28 +0700

    checkstyle2

--- Task_1_4_1/src/test/java/ru/nsu/chernikov/RecordBookTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/chernikov/RecordBookTest.java
@@ -11,14 +11,22 @@ class RecordBookTest {
     public void recordBookTest() {
 
         RecordBook recordBook = new RecordBook("Grigory", "Chernikov", false);
-        recordBook.addGrades(1, "exam", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
-        recordBook.addGrades(1, "assignment", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
-        recordBook.addGrades(1, "colloquium", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.GOOD))));
-        recordBook.addGrades(1, "test", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
-        recordBook.addGrades(2, "assignment", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.GOOD, Mark.EXCELLENT))));
-        recordBook.addGrades(2, "exam", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
-        recordBook.addGrades(3, "exam", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.GOOD))));
-        recordBook.addGrades(4, "exam", new ArrayList<>((Arrays.asList(Mark.GOOD, Mark.EXCELLENT, Mark.EXCELLENT))));
+        recordBook.addGrades(1, "exam", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
+        recordBook.addGrades(1, "assignment", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
+        recordBook.addGrades(1, "colloquium", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.GOOD))));
+        recordBook.addGrades(1, "test", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
+        recordBook.addGrades(2, "assignment", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.GOOD, Mark.EXCELLENT))));
+        recordBook.addGrades(2, "exam", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
+        recordBook.addGrades(3, "exam", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.GOOD))));
+        recordBook.addGrades(4, "exam", new ArrayList<>((Arrays.asList(
+                Mark.GOOD, Mark.EXCELLENT, Mark.EXCELLENT))));
         recordBook.setQualificationWork(5);
         System.out.println(recordBook.calculateAverage());
         System.out.println(recordBook.isItBudget());
@@ -27,14 +35,22 @@ class RecordBookTest {
         System.out.println(recordBook.getFullName());
 
         RecordBook book = new RecordBook("Ildar", "Fitkulin", true);
-        book.addGrades(1, "exam", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
-        book.addGrades(1, "assignment", new ArrayList<>((Arrays.asList(Mark.GOOD, Mark.GOOD, Mark.GOOD))));
-        book.addGrades(1, "colloquium", new ArrayList<>((Arrays.asList(Mark.SATISFACTORY, Mark.EXCELLENT, Mark.GOOD))));
-        book.addGrades(1, "test", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
-        book.addGrades(2, "assignment", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.UNSATISFACTORY, Mark.EXCELLENT))));
-        book.addGrades(2, "exam", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.UNSATISFACTORY, Mark.EXCELLENT))));
-        book.addGrades(3, "exam", new ArrayList<>((Arrays.asList(Mark.EXCELLENT, Mark.EXCELLENT, Mark.SATISFACTORY))));
-        book.addGrades(4, "exam", new ArrayList<>((Arrays.asList(Mark.GOOD, Mark.EXCELLENT, Mark.EXCELLENT))));
+        book.addGrades(1, "exam", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
+        book.addGrades(1, "assignment", new ArrayList<>((Arrays.asList(
+                Mark.GOOD, Mark.GOOD, Mark.GOOD))));
+        book.addGrades(1, "colloquium", new ArrayList<>((Arrays.asList(
+                Mark.SATISFACTORY, Mark.EXCELLENT, Mark.GOOD))));
+        book.addGrades(1, "test", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.EXCELLENT))));
+        book.addGrades(2, "assignment", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.UNSATISFACTORY, Mark.EXCELLENT))));
+        book.addGrades(2, "exam", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.UNSATISFACTORY, Mark.EXCELLENT))));
+        book.addGrades(3, "exam", new ArrayList<>((Arrays.asList(
+                Mark.EXCELLENT, Mark.EXCELLENT, Mark.SATISFACTORY))));
+        book.addGrades(4, "exam", new ArrayList<>((Arrays.asList(
+               Mark.GOOD, Mark.EXCELLENT, Mark.EXCELLENT))));
         book.setQualificationWork(4);
         System.out.println(book.calculateAverage());
         System.out.println(book.isItBudget());
